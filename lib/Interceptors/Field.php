284a0746b6072a40dede9d137d2b8add2d2c873f
Bastian Allgeier <mail@bastianallgeier.com>
2020-01-17 16:26:25 +0100

    Better field methods without case-sensitivity

--- lib/Interceptors/Field.php
+++ lib/Interceptors/Field.php
@@ -7,19 +7,32 @@ class Field extends Interceptor
 
     const CLASS_ALIAS = 'field';
 
+    public function __call($method, array $args = [])
+    {
+        if ($this->isAllowedMethod($method) === true) {
+            return $this->object->$method(...$args);
+        }
+
+        $methods = array_keys($this->object::$methods);
+        $method  = strtolower($method);
+
+        if (in_array($method, $methods) === true) {
+            return $this->object->$method(...$args);
+        }
+
+        $this->forbiddenMethod($method);
+    }
+
     public function allowedMethods(): array
     {
-        return array_merge(
-            array_keys($this->object::$methods),
-            [
-                'exists',
-                'isEmpty',
-                'isNotEmpty',
-                'key',
-                'or',
-                'value'
-            ]
-        );
+        return [
+            'exists',
+            'isEmpty',
+            'isNotEmpty',
+            'key',
+            'or',
+            'value'
+        ];
     }
 
 }
