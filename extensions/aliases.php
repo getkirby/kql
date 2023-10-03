6c147d64e37da036bf8aab9f2c66c4ee155724bc
Johann Schopplich <mail@johannschopplich.com>
2023-10-03 17:00:58 +0200

    chore: use class alias for K3 core classes

--- extensions/aliases.php
+++ extensions/aliases.php
@@ -1,4 +1,8 @@
 <?php
 
-class_alias('Kirby\Kql\Kql', 'Kql');
-class_alias('Kirby\Kql\Interceptor', 'Kirby\Kql\Interceptors\Interceptor');
+class_alias(\Kirby\Kql\Kql::class, 'Kql');
+class_alias(\Kirby\Kql\Interceptor::class, 'Kirby\Kql\Interceptors\Interceptor');
+
+// Provide backwards compatibility for Kirby 3 core classes
+class_alias(\Kirby\Kql\Interceptors\Content\Content::class, 'Kirby\Kql\Interceptors\Cms\Content');
+class_alias(\Kirby\Kql\Interceptors\Content\Field::class, 'Kirby\Kql\Interceptors\Cms\Field');
