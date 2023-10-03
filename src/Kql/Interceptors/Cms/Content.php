61259d86de402e15fc21e0a93531df46f0508ba2
Johann Schopplich <mail@johannschopplich.com>
2023-10-03 15:26:24 +0200

    chore: use cms fallback classes

---
+++ src/Kql/Interceptors/Cms/Content.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Kirby\Kql\Interceptors\Cms;
+
+use Kirby\Kql\Interceptors\Content\Content as _Content;
+
+class Content extends _Content
+{
+}
---
+++ src/Kql/Interceptors/Cms/Field.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Kirby\Kql\Interceptors\Cms;
+
+use Kirby\Kql\Interceptors\Content\Field as _Field;
+
+class Field extends _Field
+{
+}
